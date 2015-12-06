e9e9bd732ddd265890b1f271f47436ea1d4a3fe2
Chad Smith <chadxsmith@ymail.com>
2015-12-06 16:26:15 -0500

    changed price in controller to 3 cents, instead of 5 dollars....but not commited out

--- carries_cookies/app/controllers/charges_controller.rb
+++ carries_cookies/app/controllers/charges_controller.rb
@@ -6,7 +6,7 @@ class ChargesController < ApplicationController
 
 	def create
 	  # Amount in cents
-	  # @amount = 003
+	  @amount = 003
 
 	  customer = Stripe::Customer.create(
 	    :email => 'example@stripe.com',
