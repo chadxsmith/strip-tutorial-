0cdd90ac1089dc91d92e27b0b3057e69a0229ff3
Chad Smith <chadxsmith@ymail.com>
2015-12-07 21:00:33 -0500

    controller not longer carrying description and currency info --test one to charge correct amount

--- carries_cookies/app/controllers/charges_controller.rb
+++ carries_cookies/app/controllers/charges_controller.rb
@@ -6,21 +6,21 @@ class ChargesController < ApplicationController
 
 	def create
 	  # Amount in cents
-	  @amount = 100
+	  # @amount = 100
 
-	  customer = Stripe::Customer.create(
-	    :email => 'info@chadxsmith.co',
-	    :card  => params[:stripeToken]
-	  )
+	  # customer = Stripe::Customer.create(
+	  #   :email => 'info@chadxsmith.co',
+	  #   :card  => params[:stripeToken]
+	  # )
 
 	  
-	  # binding.pry
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => @amount,
-	    :description => 'Rails Stripe customer',
-	    :currency    => 'usd'
-	  )
+	  # # binding.pry
+	  # charge = Stripe::Charge.create(
+	  #   :customer    => customer.id,
+	  #   :amount      => @amount,
+	  #   :description => 'Rails Stripe customer',
+	  #   :currency    => 'usd'
+	  # )
 
 	rescue Stripe::CardError => e
 	  flash[:error] = e.message
