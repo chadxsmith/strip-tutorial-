c64ce854b4e54ea5aa845b1f4ade52f4939be37d
Chad Smith <chadxsmith@ymail.com>
2015-12-24 13:48:24 -0600

    added changes for controller and contact url

--- carries_cookies/app/controllers/charges_controller.rb
+++ carries_cookies/app/controllers/charges_controller.rb
@@ -1,37 +1,36 @@
 class ChargesController < ApplicationController
+  before_action :setup_stripe_service, only: :create
+  
+  def create
+    widget = find_widget(params[:widget])
+    token = params[:stripeToken]
+    email = params[:stripeEmail]
 
-
-	def new
-	end
-
-	def create
-
-
-	  # Amount in cents
-	  widget_id = params[:widget] #w1, w2, w3
-	  widget = widgets[widget_id.to_sym]
-	  amount = widget[:amount]
-	  token = params[:stripeToken]
-	  email = params[:stripeEmail]
-	  description = widget[:description]
-	  # @amount = 100
-
-	  customer = Stripe::Customer.create(
-	    :email => email,
-	    :card  => token
-	  )
-
-	  # binding.pry
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => amount,
-	    :description => description,
-	    :currency    => 'usd'
-	  )
-
-	rescue Stripe::CardError => e
-	  flash[:error] = e.message
-	  redirect_to charges_path
-	end
-# end
+    if widget
+      amount = widget[:amount]
+      description = widget[:description]
+      begin
+       customer = @stripe_service.create_customer(email, token)
+       charge = @stripe_service.charge_customer(customer.id, amount, description: description)
+       # redirect_to {path you want to send the user to if payment was successful} if charge.paid
+      rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to charges_path
+      end
+    else
+      
+    end
+  end
+  
+  #def new and other actions
+  
+  private
+    def setup_stripe_service
+      @stripe_service = StripeService.new
+    end
+    
+    def find_widget(widget_id)
+      # widget_id = params[:widget]
+      widget = widgets[widget_id.to_sym]
+    end
 end
---
+++ carries_cookies/app/services/stripe_service.rb
@@ -0,0 +1,17 @@
+class StripeService
+  def create_customer(email, token)
+      customer = Stripe::Customer.create(
+        :email => email,
+        :card  => token
+      )
+  end
+  
+  def charge_customer(customer_id, amount, description: "Payment made on our platform with Stripe", currency: "usd")
+    Stripe::Charge.create(
+      :customer    => customer_id,
+      :amount      => amount,
+      :description => description,
+      :currency    => currency
+    )
+  end
+end
