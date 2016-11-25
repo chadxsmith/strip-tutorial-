36fab3aa4a5b018538440e8bb0a74e3918135fdd
slava-nikulin <unicorninthecorn@gmail.com>
2016-11-25 13:22:13 +0500

    feat: add new page for convert subscribers into customers

--- carries_cookies/config/initializers/assets.rb
+++ carries_cookies/config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( bx_loader.gif )
 Rails.application.config.assets.precompile += %w( controls.png )
+Rails.application.config.assets.precompile += %w( app_simple.js )
+Rails.application.config.assets.precompile += %w( app_simple.css )
