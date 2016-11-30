62c902c7ffdbe9b4afceb090f8fcbc40be224898
slava-nikulin <unicorninthecorn@gmail.com>
2016-11-30 19:09:15 +0500

    add coupon codes for promotion01

--- carries_cookies/app/controllers/application_controller.rb
+++ carries_cookies/app/controllers/application_controller.rb
@@ -43,25 +43,28 @@ class ApplicationController < ActionController::Base
           amount: 3000,
           extras: 15,
           categoriesCount: 1,
-          categoriesDescription: " Themes"
+          categoriesDescription: " Themes",
+          coupon_id: "thirty_bucks_discount"
       },
     w2: {
           name: "Popular",
           description: "Popular Booking Package",
-          stripe_id: "popular",
+          stripe_id: "popular_60",
           amount: 6000,
           extras: 30,
           categoriesCount: 2,
-          categoriesDescription: " Themes"
+          categoriesDescription: " Themes",
+          coupon_id: "sixty_bucks_discount"
       },
     w3: {
           name: "Premier",
           description: "Premier Booking Package",
-          stripe_id: "premier",
+          stripe_id: "premier_90",
           amount: 9000,
           extras: 90,
           categoriesCount: 3,
-          categoriesDescription: " Themes"
+          categoriesDescription: " Themes",
+          coupon_id: "ninty_bucks_discount"
       }}
   end
 end
